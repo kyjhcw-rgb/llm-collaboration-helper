be021a00595450ee8e311d3e972eaacd9ad37af1
kimseokmin03 <kimseokmin20030110@gmail.com>
2026-04-06 22:04:36 +0900

    swagger_token

--- backend/src/main/java/com/capstone/collaborationhelper/config/SwaggerConfig.java
+++ backend/src/main/java/com/capstone/collaborationhelper/config/SwaggerConfig.java
@@ -1,16 +1,27 @@
 package com.capstone.collaborationhelper.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+    private static final String BEARER_AUTH = "bearerAuth";
 
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes(BEARER_AUTH, new SecurityScheme()
+                                .name("Authorization")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList(BEARER_AUTH))
                 .info(new Info()
                         .title("팀 프로젝트 협업 도우미 API")
                         .description("LLM 기반 협업 도우미 서비스의 백엔드 API 명세서")
