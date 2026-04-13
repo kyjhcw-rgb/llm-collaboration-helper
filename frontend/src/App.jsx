3a157c65f612bf8c4732925cd3074b8d39930a99
yoojeongg <youjung1558@gmail.com>
2026-04-14 01:28:08 +0900

    fe

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,27 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import Landing from './pages/Landing';
-import ProjectCreatePage from './pages/ProjectCreatePage';
-import ProjectListPage from './pages/ProjectListPage';
-import CanvasPage from './pages/CanvasPage'; 
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import ProjectListPage from "./pages/ProjectListPage";
+import ProjectCreatePage from "./pages/ProjectCreatePage";
+import CanvasPage from "./pages/CanvasPage";
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/projects/new" element={<ProjectCreatePage />} />  
-        <Route path="/lobby" element={<ProjectListPage />} />
-        <Route path="/canvas" element={<CanvasPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+export default function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Navigate to="/login" replace />} />
+
+                {/* 인증 페이지 */}
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/signup" element={<SignupPage />} />
+
+                {/* 프로젝트 관리 페이지 */}
+                <Route path="/projects" element={<ProjectListPage />} />
+                <Route path="/projects/new" element={<ProjectCreatePage />} />
+
+                {/* 다이어그램 캔버스 (에디터) 페이지 */}
+                <Route path="/canvas" element={<CanvasPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
-
-export default App;
